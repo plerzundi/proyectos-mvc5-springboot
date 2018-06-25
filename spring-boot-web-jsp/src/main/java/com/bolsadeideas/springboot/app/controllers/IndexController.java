2d0c9b500c858bf956d6db082529219e71fa58b8
Patricio Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-06-25 02:52:16 -0400

    Init proyectos mvc5 springboot

---
+++ spring-boot-web-jsp/src/main/java/com/bolsadeideas/springboot/app/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@Value("${application.controller.titulo}")
+	private String titulo;
+	
+	
+	@GetMapping("/index")
+	public String Index(Model model) {
+		model.addAttribute("titulo",this.titulo);
+		return "index";
+	}
+	
+}
---
+++ spring-boot-web-thymeleaf/src/main/java/com/bolsadeideas/springboot/thymeleaf/app/SpringBootWebThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.thymeleaf.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebThymeleafApplication.class, args);
+	}
+}
