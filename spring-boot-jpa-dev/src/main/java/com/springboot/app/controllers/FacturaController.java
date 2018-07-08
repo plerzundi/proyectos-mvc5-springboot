10c12ee87fda243e87313a731d0db642b8256191
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-07-08 18:24:11 -0400

    Factura controller
    Creacion factura controller

---
+++ spring-boot-jpa-dev/src/main/java/com/springboot/app/controllers/FacturaController.java
@@ -0,0 +1,49 @@
+package com.springboot.app.controllers;
+
+import com.springboot.app.models.entity.Cliente;
+import com.springboot.app.models.entity.Factura;
+import com.springboot.app.models.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+
+    @Autowired
+    private IClienteService clienteService;
+
+
+    @GetMapping("/form/{clienteId}")
+    public String crear(@PathVariable(value = "clienteId") Long clienteId,
+                        Map<String, Object> model,
+                        RedirectAttributes flash) {
+
+        Cliente cliente = clienteService.findOne(clienteId);
+        if(cliente == null){
+            flash.addAttribute("error","El cliente no existe en la bases de datos");
+            return "redirect:/listar";
+        }
+
+        Factura factura = new Factura();
+        factura.setCliente(cliente);
+        model.put("factura",factura);
+        model.put("titulo","Crear Factura");
+
+        return "factura/form";
+    }
+
+
+
+
+
+
+}
