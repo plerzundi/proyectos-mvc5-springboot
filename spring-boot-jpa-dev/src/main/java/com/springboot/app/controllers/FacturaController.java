ee1a41062169bda98a3141448ece3acd1226036d
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-07-10 00:43:54 -0400

    Guardar Factura
    Se  crea método para añadir factura al cliente.

--- spring-boot-jpa-dev/src/main/java/com/springboot/app/controllers/FacturaController.java
+++ spring-boot-jpa-dev/src/main/java/com/springboot/app/controllers/FacturaController.java
@@ -2,11 +2,15 @@ package com.springboot.app.controllers;
 
 import com.springboot.app.models.entity.Cliente;
 import com.springboot.app.models.entity.Factura;
+import com.springboot.app.models.entity.ItemFactura;
 import com.springboot.app.models.entity.Producto;
 import com.springboot.app.models.service.IClienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -19,6 +23,8 @@ public class FacturaController {
 
     @Autowired
     private IClienteService clienteService;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
 
     @GetMapping("/form/{clienteId}")
@@ -44,6 +50,29 @@ public class FacturaController {
     @GetMapping(value = "/cargar-productos/{term}",produces = {"application/json"})
     public @ResponseBody  List<Producto> cargarProductos(@PathVariable String term){
         return clienteService.findByNombre(term);
+    }
+
+    @PostMapping("/form")
+    public String guardar(Factura factura, @RequestParam(name = "item_id[]",required = false)Long[] itemId,
+                          @RequestParam(name = "cantidad[]",required = false) Integer[] cantidad,
+                          RedirectAttributes flash, SessionStatus status){
+
+        for(int i=0; i <itemId.length;i++){
+            Producto producto = clienteService.findProductoById(itemId[i]);
+
+            ItemFactura linea = new ItemFactura();
+            linea.setCantidad(cantidad[i]);
+            linea.setProducto(producto);
+            factura.addItemFactura(linea);
+            log.info("ID:"+itemId[i].toString()+", cantidad: "+cantidad[i].toString());
+        }
+
+            clienteService.saveFactura(factura);
+            status.setComplete();
+
+            flash.addAttribute("success","Factura creada con éxito!");
+
+        return "redirect:/ver/"+factura.getCliente().getId();
     }
 
 
