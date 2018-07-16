0101f2c874707c5ef8e45f8003086497d8213e3c
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-07-16 04:39:00 -0400

    INSERT  users && authorities
    Se añade sentencia del archivo import.sql para la creacion de las siguientes tablas entity.

--- spring-boot-jpa-dev/src/main/resources/import.sql
+++ spring-boot-jpa-dev/src/main/resources/import.sql
@@ -43,3 +43,12 @@ INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1, 1, 7);
 
 INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, NOW());
 INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3, 2, 6);
+
+
+INSERT INTO users (username, password, enabled) VALUES ('plerzundi','$2a$10$JVVZO.SZoqhGRcuDdAjPsOgh9UMNE4AFpG3khKT6ZYLmdS66IH1b2',1);
+INSERT INTO users (username, password, enabled) VALUES ('admin','$2a$10$K1OcSBKXuRfmdw8d5l2iVenofMgNFk4UKPe2uB8n9N9RjR.uZXBau',1);
+
+INSERT INTO authorities (user_id, authority) VALUES (1,'ROLE_USER');
+INSERT INTO authorities (user_id, authority) VALUES (2,'ROLE_ADMIN');
+INSERT INTO authorities (user_id, authority) VALUES (2,'ROLE_USER');
+
