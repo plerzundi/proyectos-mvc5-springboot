44347026e1c55b8bf1ef9f9a47d51d59f2ddeb69
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-07-17 00:51:26 -0400

    Configuración locale properties
    Se añade mas funcionalidades a la web app, implementando distintos idiomas a la aplicación.

--- spring-boot-jpa-dev/src/main/java/com/springboot/app/MvcConfig.java
+++ spring-boot-jpa-dev/src/main/java/com/springboot/app/MvcConfig.java
@@ -3,8 +3,14 @@ package com.springboot.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 
 @Configuration
@@ -36,4 +42,28 @@ public class MvcConfig  implements WebMvcConfigurer {
 
 
 
+     /*
+     * Parametro de lenguaje
+     * */
+
+     @Bean
+     public LocaleResolver localeResolver(){
+         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+         localeResolver.setDefaultLocale(new Locale("es","ES"));
+         return localeResolver;
+     }
+
+     @Bean
+     public LocaleChangeInterceptor localeChangeInterceptor(){
+
+         LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+         localeInterceptor.setParamName("lang");
+         return localeInterceptor;
+     }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor( localeChangeInterceptor());
+    }
 }
